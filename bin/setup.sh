31ba945c47b37eeef954d7edf76da86d14422665
schetnikovich <tshcetnikovich@gmail.com>
2015-12-05 22:39:02 +0300

    Main setup script now includes ideavim

--- bin/setup.sh
+++ bin/setup.sh
@@ -6,3 +6,4 @@ ENV_DIR=$(dirname $BIN_DIR)                                       # Get root env
 $ENV_DIR/vim/bin/setup.sh
 $ENV_DIR/vifm/bin/setup.sh
 $ENV_DIR/tmux/bin/setup.sh
+$ENV_DIR/ideavim/bin/setup.sh
